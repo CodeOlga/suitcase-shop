938b521ea4b2754f72eafaa8b51b35729df98fd0
olgakrv <111949555+CodeOlga@users.noreply.github.com>
2025-10-07 18:33:40 +0300

    fix roots

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -14,14 +14,47 @@ export function fillTemplate(tpl, map) {
   return out;
 }
 
+// export function renderProducts(products, container, tpl, addToCart) {
+//   products.forEach((p) => {
+//     const html = fillTemplate(tpl, {
+//       id: p.id,
+//       name: p.name,
+//       price: p.price,
+//       img1x: `${BASE}/${p.imageUrl}`,
+//       img2x: `${BASE}/${(p.imageUrl || "").replace("@1x", "@2x")}`,
+//       badge: p.salesStatus ? `<span class="badge">SALE</span>` : "",
+//     });
+
+//     const wrapper = document.createElement("div");
+//     wrapper.innerHTML = html.trim();
+//     const card = wrapper.firstElementChild;
+
+//     card.addEventListener("click", (e) => {
+//       if (e.target.closest(".add-to-cart")) return;
+//       window.location.href = `${BASE}/pages/product-details-template.html?id=${p.id}`;
+//     });
+
+//     card.querySelector(".add-to-cart").addEventListener("click", (e) => {
+//       e.stopPropagation();
+//       addToCart(p, 1);
+//     });
+
+//     container.appendChild(card);
+//   });
+// }
+
 export function renderProducts(products, container, tpl, addToCart) {
   products.forEach((p) => {
+    // прибираємо "./" тільки на початку шляхів з data.json
+    const img = (p.imageUrl || "").replace(/^\.\//, "");
+    const img2 = img.replace("@1x", "@2x");
+
     const html = fillTemplate(tpl, {
       id: p.id,
       name: p.name,
       price: p.price,
-      img1x: `${BASE}/${p.imageUrl}`,
-      img2x: `${BASE}/${(p.imageUrl || "").replace("@1x", "@2x")}`,
+      img1x: `${BASE}/${img}`,
+      img2x: `${BASE}/${img2}`,
       badge: p.salesStatus ? `<span class="badge">SALE</span>` : "",
     });
 
