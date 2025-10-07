e462fd095fa697baabdae3203c0580566270608f
olgakrv <111949555+CodeOlga@users.noreply.github.com>
2025-10-07 18:57:42 +0300

    fix Netlify src paths

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -1,10 +1,18 @@
 // export const BASE = window.location.pathname.includes("/pages/") ? ".." : ".";
 
-export const BASE = window.location.hostname.includes("netlify.app")
-  ? ""
-  : window.location.pathname.includes("/pages/")
-  ? ".."
-  : ".";
+// export const BASE = window.location.hostname.includes("netlify.app")
+//   ? ""
+//   : window.location.pathname.includes("/pages/")
+//   ? ".."
+//   : ".";
+
+export const BASE =
+  window.location.hostname.includes("netlify.app") ||
+  window.location.hostname.includes("github.io")
+    ? "/src"
+    : window.location.pathname.includes("/pages/")
+    ? ".."
+    : ".";
 
 export function fillTemplate(tpl, map) {
   let out = tpl;
